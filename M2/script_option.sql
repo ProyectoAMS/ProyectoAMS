d19b7c47d1a7d50304b65d7e8153ed8ba6df0c72
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2022-01-18 10:30:25 +0100

    Add files via upload

---
+++ M2/script_option.sql
@@ -0,0 +1,27 @@
+#HISTORIA 1
+
+insert  into  ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(101,'Entres dins?',0,102,101,10);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(102,'Et quesdes alla?',0,103,101,10);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(103,'Puges la escala?',0,104,102,10);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(104,'Passes per les portes batents?',0,105,102,10);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(105,'Entres al armari?',0,106,105,10);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(106,'Entres a un passadís sota la casa',0,107,105,10);
+
+#HISTORIA 2
+
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(111,'Anar?',0,112,111,11);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(112,'No anar',0,113,111,11);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(113,'Ajudar',0,114,112,11);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(114,'No ajudar',0,115,112,11);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(115,'Donar',0,116,115,11);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(116,'No donar',0,117,115,11);
+
+
+#historia 3
+
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(121,'Anar',0,122,121,12);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(122,'No anar',0,123,121,12);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(123,'Una casa abandonada',0,124,122,12);
+insert into ams.option(ID_OPTION,option_description,answer,go_step,FK_STEP_ID_STEP,FK_STEP_ADVENTURE_ID_ADVENTURE) value(124,'Continuar corrent ',0,125,122,12);
+
+
